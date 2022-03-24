0aebe5cf9d64424b0b8c07b0dcd9f6bed9515091
Quintin Willison <q@qwuk.net>
2022-03-24 16:34:38 +0000

    Add ESLint, with Airbnb base, fixing existing transgressions.

--- test/agents.test.js
+++ test/agents.test.js
@@ -1,23 +1,28 @@
-const fs = require('fs');
 const path = require('path');
 const { Validator } = require('jsonschema');
 
 const schemaPath = path.resolve(__dirname, '..', 'json-schemas', 'src', 'agents.json');
 const agentsPath = path.resolve(__dirname, '..', 'protocol', 'agents.json');
 const versionsPath = path.resolve(__dirname, '..', 'json-schemas', 'versions.json');
+
+// eslint-disable-next-line import/no-dynamic-require
 const schema = require(schemaPath);
+
+// eslint-disable-next-line import/no-dynamic-require
 const agents = require(agentsPath);
+
+// eslint-disable-next-line import/no-dynamic-require
 const versions = require(versionsPath);
 
 const jsonschema = new Validator();
 jsonschema.addSchema(
-    schema,
-    `https://schemas.ably.com/json/agents-${versions['agents']}.json`
+  schema,
+  `https://schemas.ably.com/json/agents-${versions.agents}.json`,
 );
 
-describe("agents.json", () => {
-    it("matches the jsonschema", () => {
-        const { errors } = jsonschema.validate(agents, schema);
-        expect(errors.length).toBe(0);
-    });
+describe('agents.json', () => {
+  it('matches the jsonschema', () => {
+    const { errors } = jsonschema.validate(agents, schema);
+    expect(errors.length).toBe(0);
+  });
 });
