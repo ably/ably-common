42539b52fa04880cb8889b99104b980ae8109341
Quintin Willison <q@qwuk.net>
2022-05-16 08:55:59 +0100

    Restructure the ably-java SDK manifest to introduce a root layer of indirection.
    This allows us to have keys in this document that don't release 1:1 to the feature compliance tree.
    The first such key is variants.

--- features/manifest.js
+++ features/manifest.js
@@ -1,9 +1,14 @@
 const { Properties } = require('./sdk-node-properties');
 
+const COMPLIANCE_KEY = 'compliance';
+
 class Manifest {
   constructor(manifest) {
     if (!(manifest instanceof Map)) {
       throw new Error('manifest should be a Map.');
+    }
+    if (!(manifest.get(COMPLIANCE_KEY) instanceof Map)) {
+      throw new Error('manifest compliance should be a Map.');
     }
     this.manifest = manifest;
   }
@@ -15,7 +20,7 @@ class Manifest {
    * @returns {Map} The feature node.
    */
   find(featurePath) {
-    let node = this.manifest;
+    let node = this.manifest.get(COMPLIANCE_KEY);
     featurePath.forEach((featurePathComponent) => {
       if (node) {
         node = node.get(featurePathComponent);
